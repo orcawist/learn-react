987dfc6add1bb50c6fdc62ad1490e97b27f5aea9
yoruwis <yoruwis@gmail.com>
2022-10-31 16:29:22 +0900

    🎉 First commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "prettier"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        // "semi": ["error", "always"],
+        // "quotes": ["error", "double"],
+        "react/prop-types": "off"
+    },
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    }
+};
---
+++ components/Headline.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+export function Headline(props) {
+    return (
+        <div>
+            <h1 className={styles.title}>
+                {props.page} Page
+            </h1>
+
+            <p className={styles.description}>
+                Get started by editing{" "}
+                {props.children}
+            </p>
+        </div>
+
+
+    );
+}
