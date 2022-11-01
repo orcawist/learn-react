dedcf366a150f15714ce12f4b508c6f30e401535
yoruwis <yoruwis@gmail.com>
2022-11-02 08:49:39 +0900

    ✨ useStateで配列

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,15 +9,31 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
-  });
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (text.length > 5) return;
+    setText(e.target.value);
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じのがある");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -40,14 +56,13 @@ export default function Home() {
 
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => {
-          if (text.length > 5) return;
-          setText(e.target.value);
-        }}
-      />
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
 
       <Main page="index" />
 
