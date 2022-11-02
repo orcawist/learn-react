31179317e98ba42cbceb764c9d02a5cd2e9a4b0f
yoruwis <yoruwis@gmail.com>
2022-11-02 10:09:49 +0900

    ✨ ページ間のstateのリフトアップ

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,20 @@
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightblue } from "src/hooks/useBgLightblue";
 import "src/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const counter = useCounter();
+  const inputArray = useInputArray();
+  useBgLightblue();
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} {...counter} {...inputArray} />
     </>
   );
 }
