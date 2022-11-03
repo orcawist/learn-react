b4fa3b0b233e0d1fc640822eba7f6e339a6e31f6
yoruwis <yoruwis@gmail.com>
2022-11-03 18:10:18 +0900

    🚸 APIを叩く時のローディング、エラー状態

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,10 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
-import { Footer } from "src/components/Footer";
-import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import Posts from "src/components/Posts";
 
 const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-    console.log(posts);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
   return (
     <div className={styles.container}>
       <Head>
@@ -26,13 +13,7 @@ const Home = (props) => {
       </Head>
       <Header />
 
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </div>
   );
 };
