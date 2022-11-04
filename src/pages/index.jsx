2ea53e89fa033a245069fc06aea26a752ba04b75
yoruwis <yoruwis@gmail.com>
2022-11-04 10:29:30 +0900

    🔥 indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
-import { Footer } from "src/components/Footer";
-import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
-const Index = (props) => {
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,23 +13,8 @@ const Index = (props) => {
 
       <Header />
 
-      {props.isShow ? <h2>{props.doubleCount}</h2> : null}
-
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Main page="Index" />
-
-      <Footer />
+      <h1>Next.js</h1>
+      <p>JSONPlaceholder</p>
     </div>
   );
 };
