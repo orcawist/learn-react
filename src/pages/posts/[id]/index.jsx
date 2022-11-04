2653c03d3a8524f80704516f5d99b9390d9d17f6
yoruwis <yoruwis@gmail.com>
2022-11-04 10:39:43 +0900

    🚚 投稿系ページの名前変更

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -4,7 +4,7 @@ import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 import Post from "src/components/Post";
 
-const PostId = () => {
+const PostsId = () => {
   return (
     <div className={styles.container}>
       <Header />
@@ -14,4 +14,4 @@ const PostId = () => {
   );
 };
 
-export default PostId;
+export default PostsId;
