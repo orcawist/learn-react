eca90d75c477367c85e5543c90c5f02cd5deddb7
yoruwis <yoruwis@gmail.com>
2022-11-04 10:32:17 +0900

    🔥 不要なファイル、コード削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,14 @@
 import Head from "next/head";
-import React, { useEffect, useState } from "react";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgColor } from "src/hooks/useBgColor";
+import React from "react";
 import "src/styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArray} />
+      <Component {...pageProps} />
     </>
   );
 };
