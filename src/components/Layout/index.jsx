ef38da0030df8a4bfa5edf276421202019f490e7
yoruwis <yoruwis@gmail.com>
2022-11-06 19:50:03 +0900

    ➕ Tailwind CSSセットアップ

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Classes from "src/components/Layout/Layout.module.css";
 
 export const Layout = (props) => {
   return <div className={Classes.container}>{props.children}</div>;
