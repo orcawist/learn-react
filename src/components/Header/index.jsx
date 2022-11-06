e0e460457eb06db9b2571591b236b340f03d2d03
yoruwis <yoruwis@gmail.com>
2022-11-06 20:22:10 +0900

    💄 Tailwind CSS適用

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,10 +10,14 @@ const NAV_ITEMS = [
 
 export const Header = () => {
   return (
-    <header>
+    <header className="flex justify-center items-center border-b w-full h-24">
       {NAV_ITEMS.map((item) => {
         return (
-          <Link key={item.href} href={item.href}>
+          <Link
+            key={item.href}
+            href={item.href}
+            className="inline-block py-2 px-6 text-xl hover:text-blue-500 focus:text-blue-500 active:text-blue-500"
+          >
             <p>{item.label}</p>
           </Link>
         );
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Layout = (props) => {
-  return <div className={Classes.container}>{props.children}</div>;
+  return (
+    <div className="flex flex-col items-center px-2 mx-auto max-w-2xl min-h-screen">
+      {props.children}
+    </div>
+  );
 };
