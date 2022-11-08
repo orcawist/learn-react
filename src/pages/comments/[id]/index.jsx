33e3ef87a2b61f6fd6e0c7339731382611c681ef
yoruwis <yoruwis@gmail.com>
2022-11-08 15:46:09 +0900

    🚧 comments/[id]をSGにしたい

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,13 +1,46 @@
 import React from "react";
 import { Comment } from "src/components/Comment";
 import { Header } from "src/components/Header";
+import { SWRConfig } from "swr";
 
-const CommentId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => {
+    return { params: { id: comment.id.toString() } };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comment/${id}`;
+  const comments = await fetch(COMMENT_API_URL);
+  const commentsData = await comments.json();
+
+  console.log(`${id}のページをSG`);
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentsData,
+      },
+    },
+  };
+};
+
+const CommentId = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Header />
 
-      <Comment />
+      <SWRConfig value={{ fallback }}>
+        <Comment />
+      </SWRConfig>
     </div>
   );
 };
